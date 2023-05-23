464c5472d25e07b1bb6769adbad0f3b7595e976c
David Cohen <deiv464@gmail.com>
2023-05-23 23:26:59 +0300

    add documentation

--- include/Animal.h
+++ include/Animal.h
@@ -3,21 +3,96 @@
 
 using std::string, std::cout, std::endl;
 
-class Animal 
+/**
+ * @brief The Animal class represents an abstract base class for animals.
+ *
+ * This class provides common functionality and attributes for different types of animals.
+ * It includes methods for moving, changing direction, and managing the animal's location.
+ */
+class Animal
 {
 protected:
-	string _name;
-	Location _loc, _next_step;
-	bool _isStop;
+	std::string _name; ///< The name of the animal
+	Location _loc; ///< The current location of the animal
+	Location _next_step; ///< The next step or target location of the animal
+	bool _isStop; ///< A flag indicating whether the animal is stopped or not
+
+	/**
+	 * @brief Sets the location of the animal.
+	 *
+	 * This method is a pure virtual function that should be implemented by derived classes
+	 * to set the location of the animal based on its specific behavior.
+	 */
 	virtual void setLocation() = 0;
+
 public:
-	Animal(const string& name, const Location& loc);
-	virtual void printDetails()const = 0;
-	virtual char getInitial()const = 0;
+	/**
+	 * @brief Constructs an Animal object with the given name and location.
+	 *
+	 * @param name The name of the animal.
+	 * @param loc The initial location of the animal.
+	 */
+	Animal(const std::string& name, const Location& loc);
+
+	/**
+	 * @brief Prints the details of the animal.
+	 *
+	 * This is a pure virtual function that should be implemented by derived classes
+	 * to print the specific details of the animal.
+	 */
+	virtual void printDetails() const = 0;
+
+	/**
+	 * @brief Gets the initial character representation of the animal.
+	 *
+	 * This is a pure virtual function that should be implemented by derived classes
+	 * to return the initial character representation of the animal.
+	 *
+	 * @return The initial character representation of the animal.
+	 */
+	virtual char getInitial() const = 0;
+
+	/**
+	 * @brief Stops the animal.
+	 *
+	 * This is a pure virtual function that should be implemented by derived classes
+	 * to stop the animal's movement or behavior.
+	 */
 	virtual void stop() = 0;
+
+	/**
+	 * @brief Moves the animal to the next step or target location.
+	 *
+	 * This method updates the animal's current location to the next step or target location.
+	 */
 	virtual void step();
+
+	/**
+	 * @brief Moves the animal to its next step or target location.
+	 *
+	 * This method moves the animal from its current location to the next step or target location.
+	 * It calls the `setLocation` method to update the animal's location based on its specific behavior.
+	 */
 	void move();
-	Location getLocation()const;
+
+	/**
+	 * @brief Gets the current location of the animal.
+	 *
+	 * @return The current location of the animal.
+	 */
+	Location getLocation() const;
+
+	/**
+	 * @brief Turns the animal horizontally.
+	 *
+	 * This method changes the direction of the animal to face horizontally.
+	 */
 	void turnHorizontally();
+
+	/**
+	 * @brief Turns the animal vertically.
+	 *
+	 * This method changes the direction of the animal to face vertically.
+	 */
 	void turnVertically();
 };
--- include/Zoo.h
+++ include/Zoo.h
@@ -1,13 +1,13 @@
 #pragma once
-#include"Lion.h"
-#include"Dog.h"
-#include"Clownfish.h"
-#include"Goose.h"
-#include"Monkey.h"
-#include"Owl.h"
-#include"Shark.h"
-#include<vector>
-#include<string>
+#include "Lion.h"
+#include "Dog.h"
+#include "Clownfish.h"
+#include "Goose.h"
+#include "Monkey.h"
+#include "Owl.h"
+#include "Shark.h"
+#include <vector>
+#include <string>
 #include <algorithm>
 #include <map>
 #include <unordered_map>
@@ -15,28 +15,106 @@
 
 using std::unique_ptr, std::make_unique;
 
+/**
+ * @brief The Zoo class represents a zoo that manages various types of animals.
+ *
+ * This class provides functionality to interact with animals in the zoo.
+ * It allows creating, deleting, moving, and stopping animals, as well as providing help and information about the zoo.
+ */
 class Zoo
 {
-	std::vector<unique_ptr<Animal>> _animals;
-	std::unordered_map<std::string_view, std::function<void()>> _commandMap;
+	std::vector<unique_ptr<Animal>> _animals; ///< The collection of animals in the zoo
+	std::unordered_map<std::string_view, std::function<void()>> _commandMap; ///< The map of commands and their corresponding functions
+
 	enum AnimalType
 	{
 		CLOWNFISH, DOG, GOOSE, LION, MONKEY, OWL, SHARK
 	};
 
+	/**
+	 * @brief Stops the animal at the specified index.
+	 *
+	 * @param index The index of the animal in the collection to stop.
+	 */
 	void stop(int index);
+
+	/**
+	 * @brief Moves the animal at the specified index.
+	 *
+	 * @param index The index of the animal in the collection to move.
+	 */
 	void move(int index);
+
+	/**
+	 * @brief Moves all animals in the zoo by one step.
+	 */
 	void step();
+
+	/**
+	 * @brief Creates an animal of the specified type with the given name.
+	 *
+	 * @param type The type of the animal to create.
+	 * @param name The name of the animal.
+	 */
 	void create(AnimalType type, string name);
+
+	/**
+	 * @brief Deletes the animal at the specified index.
+	 *
+	 * @param index The index of the animal in the collection to delete.
+	 */
 	void del(int index);
+
+	/**
+	 * @brief Deletes all animals of the specified type.
+	 *
+	 * @param type The type of the animals to delete.
+	 */
 	void delAll(AnimalType type);
+
+	/**
+	 * @brief Prints the available commands and their descriptions.
+	 */
 	void help() const;
-	void printMap()const;
+
+	/**
+	 * @brief Prints the Zoo map.
+	 */
+	void printMap() const;
+
+	/**
+	 * @brief Prints the Zoo list.
+	 */
 	void printList() const;
-	AnimalType toAnimalType(const std::string& str)const;
+
+	/**
+	 * @brief Converts a string to an AnimalType value.
+	 *
+	 * @param str The string representation of the animal type.
+	 * @return The corresponding AnimalType value.
+	 */
+	AnimalType toAnimalType(const std::string& str) const;
+
+	/**
+	 * @brief Converts a string to lowercase.
+	 *
+	 * @param s The string to convert to lowercase.
+	 */
 	void toLower(string& s) const;
+
+	/**
+	 * @brief Sets up the command map with available commands and their corresponding functions.
+	 */
 	void setCommandMap();
+
 public:
+	/**
+	 * @brief Constructs a Zoo object.
+	 */
 	Zoo();
+
+	/**
+	 * @brief Runs the zoo application.
+	 */
 	void run();
 };
